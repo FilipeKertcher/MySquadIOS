557ca95ef42b3499567caa610ca35a63a923b289
Filipe Kertcher <filipekertcher97@gmail.com>
2020-07-19 20:52:50 -0300

    Initial Commit

---
+++ MySquad/MySquad.h
@@ -0,0 +1,19 @@
+//
+//  MySquad.h
+//  MySquad
+//
+//  Created by Filipe Kertcher on 19/07/20.
+//  Copyright © 2020 Filipe Kertcher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MySquad.
+FOUNDATION_EXPORT double MySquadVersionNumber;
+
+//! Project version string for MySquad.
+FOUNDATION_EXPORT const unsigned char MySquadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MySquad/PublicHeader.h>
+
+
