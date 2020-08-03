2601ca67d022508559d3d1d460fb7dc0309573b1
Filipe Kertcher <filipekertcher97@gmail.com>
2020-08-02 21:49:40 -0300

    tests: ensure signup presenter shows error if no name is provided

--- PresentationTests/SignupPresenterTests.swift
+++ PresentationTests/SignupPresenterTests.swift
@@ -7,14 +7,70 @@
 //
 
 import XCTest
-@testable import Presentation
+
+
+struct SignupViewModel {
+      var name: String?;
+      var email : String?;
+      var socialMediaType: String?;
+      var socialMediaToken: String?;
+}
+
+class SignUpPresenter {
+    
+    private let alertView: AlertView
+    
+    init(alertView:AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signup(viewModel:SignupViewModel){
+        if viewModel.name == nil || viewModel.name!.isEmpty {
+            alertView.showMessage(viewModel:AlertViewModel(title:"Falha na validação",message:"O Campo nome é obrigatório"))
+        }
+        
+    }
+    
+    
+}
+
+protocol AlertView {
+    func showMessage(viewModel:AlertViewModel)
+}
+
+struct AlertViewModel : Equatable {
+    var title:String;
+    var message:String
+}
 
 class SignupPresenterTests: XCTestCase {
 
-    override func test_() {
+    func test_signup_show_error_mesasge_if_name_is_not_provided() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy )
+        let signUpViewModel = SignupViewModel(email:"123",socialMediaType:"facebook",socialMediaToken:"123")
+        
+        sut.signup(viewModel: signUpViewModel)
+        
+        XCTAssertEqual(alertViewSpy.viewModel,AlertViewModel(title:"Falha na validação",message:"O Campo nome é obrigatório"))
     }
 
      
 
+}
+
+extension SignupPresenterTests {
+    class AlertViewSpy : AlertView {
+        
+        var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel:AlertViewModel){
+            self.viewModel = viewModel
+        }
+        
+        
+        
+        
+    }
 }
