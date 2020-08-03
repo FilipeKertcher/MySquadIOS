bedc2b8e218bf017ae79f875852bfd9c682d7019
Filipe Kertcher <filipekertcher97@gmail.com>
2020-08-02 21:30:29 -0300

    feat: started presentation layer

---
+++ PresentationTests/SignupPresenterTests.swift
@@ -0,0 +1,20 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by Filipe Kertcher on 02/08/20.
+//  Copyright © 2020 Filipe Kertcher. All rights reserved.
+//
+
+import XCTest
+@testable import Presentation
+
+class SignupPresenterTests: XCTestCase {
+
+    override func test_() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+     
+
+}
